51d49f11c46a196c06562fb6607b9e05ad73c698
Joe Wandy <joewandy@gmail.com>
2015-04-08 22:19:02 +0700

    add a lot of data files
    Former-commit-id: a2267e19c0359ceec9dc7f80efdffc8364de1c28

---
+++ lda_batch.py
@@ -0,0 +1,51 @@
+from lda_for_fragments import *
+    
+n_topics = 50
+
+# beer positive mode
+# run_lda('beer_pos', 'input/QC_Beer_ParentsFragments_Tonyscripting.csv', True, n_topics)
+# run_lda('beer_pos', 'input/QC_Beer_ParentsFragments_Tonyscripting.csv', False, n_topics)
+
+# beer negative mode
+run_lda('beer_neg', 'input/QCBeerT10_neg.csv', True, n_topics)
+run_lda('beer_neg', 'input/QCBeerT10_neg.csv', False, n_topics)
+
+# urine37 mixed modes
+# run_lda('urine37_mixed', 'input/Urine_FragmentsParents_MS2filter5000_Tonyscripting.csv', True, n_topics)
+# run_lda('urine37_mixed', 'input/Urine_FragmentsParents_MS2filter5000_Tonyscripting.csv', False, n_topics)
+
+# urine37 positive mode
+run_lda('urine37_pos', 'input/Urine37_T10mixed_pos.csv', True, n_topics)
+run_lda('urine37_pos', 'input/Urine37_T10mixed_pos.csv', False, n_topics)
+
+# urine37 negative mode
+run_lda('urine37_neg', 'input/Urine37_T10mixed_neg.csv', True, n_topics)
+run_lda('urine37_neg', 'input/Urine37_T10mixed_neg.csv', False, n_topics)
+
+# campylobacter positive mode
+run_lda('campy_pos', 'input/CampyT10_pos.csv', True, n_topics)
+run_lda('campy_pos', 'input/CampyT10_pos.csv', False, n_topics)
+
+# campylobacter negative mode
+run_lda('campy_neg', 'input/CampyT10_neg.csv', True, n_topics)
+run_lda('campy_neg', 'input/CampyT10_neg.csv', False, n_topics)
+
+# urine94 mixed modes
+# run_lda('urine94_mixed', 'input/ParentFragmentMatrix_Urine94_mixed.csv', True, n_topics)
+# run_lda('urine94_mixed', 'input/ParentFragmentMatrix_Urine94_mixed.csv', False, n_topics)
+
+# urine94 positive mode
+run_lda('urine94_pos', 'input/Urine94_T10mixed_pos.csv', True, n_topics)
+run_lda('urine94_pos', 'input/Urine94_T10mixed_pos.csv', False, n_topics)
+
+# urine94 negative mode
+run_lda('urine94_neg', 'input/Urine94_T10mixed_neg.csv', True, n_topics)
+run_lda('urine94_neg', 'input/Urine94_T10mixed_neg.csv', False, n_topics)
+
+# some ecoli thing?
+run_lda('Ecoli_APEC_pos', 'input/Ecoli_APEC_WC_T10_pos.csv', True, n_topics)
+run_lda('Ecoli_APEC_pos', 'input/Ecoli_APEC_WC_T10_pos.csv', False, n_topics)
+
+# some ecoli thing?
+run_lda('Ecoli_NISSLE_pos', 'input/Ecoli_NISSLE_WC_T10_pos.csv', True, n_topics)
+run_lda('Ecoli_NISSLE_pos', 'input/Ecoli_NISSLE_WC_T10_pos.csv', False, n_topics)
