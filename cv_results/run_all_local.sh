6d679374a00f6ac785cf37c5412eb78c5f3362cb
Joe Wandy <joewandy@gmail.com>
2015-07-07 23:44:02 +0100

    add script to run local cv
    Former-commit-id: 5ef354b155510ecfaefd231760f7b2fbe46052b5
    Former-commit-id: 50950497726e52e2deb8632694652e9181605a58

---
+++ cv_results/run_all_local.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd beer3pos.is
+./do_cv_local.sh
+
+cd ../urine37pos.is
+./do_cv_local.sh
+
+cd ../beer3pos.is.3bags
+./do_cv_local.sh
+
+cd ../urine37pos.is.3bags
+./do_cv_local.sh
+
