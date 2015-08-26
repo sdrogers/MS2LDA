removed old cross-validation results, fixed underflow in calculation of log evidence of multinomial mixture using the logsumexp trick
Former-commit-id: 96d429669662e708af606483d83d4c72318b5a27
Former-commit-id: dab869d42f4ad454d9ce676478fbbf0afeddae7f

mixture/mixture_cgs.py
@@ -1,15 +1,14 @@
 from collections import namedtuple
-from numpy.random import RandomState
 import sys
 import time
 
-from numpy import int32
-from scipy.special import gammaln
-
-from mixture_generate_data import MixtureDataGenerator
-import justin.lda_utils as utils
 import numpy as np
+from numpy import int32
+from numpy.random import RandomState
+from scipy.misc import logsumexp
 
+import justin.lda_utils as utils
+from mixture_generate_data import MixtureDataGenerator
 
 Sample = namedtuple('Sample', 'cdk ckn')
 
@@ -109,15 +108,14 @@ class CollapseGibbsMixture(object):
             doc_word_counts = document.values.flatten()
             nnz_counts = doc_word_counts[nnz]
             n_words += np.sum(nnz_counts)
-            doc_marg = 0
+            doc_marg = np.zeros(self.K)
             for k in range(self.K):
                 temp = 0
                 for n in nnz:
                     curr_word_count = doc_word_counts[n]
                     temp += curr_word_count * np.log(phi[k, n])
-                temp = np.exp(temp)
-                doc_marg += theta[k] * temp
-            marg += np.log(doc_marg)
+                doc_marg[k] = np.log(theta[k]) + temp
+            marg += logsumexp(doc_marg)
         perp = np.exp(-(marg/n_words))
         return marg, perp        
             
@@ -260,6 +258,10 @@ def main():
     mixture = CollapseGibbsMixture(df, vocab, n_cluster, alpha, beta, random_state=random_state)
     start_time = time.time()
     mixture.run(n_burn, n_samples, n_thin, use_native=True)
+    print "margs"
+    print mixture.margs
+    print "perps"
+    print mixture.perps
     print("--- TOTAL TIME %d seconds ---" % (time.time() - start_time))
     mixture.print_topic_words()
     