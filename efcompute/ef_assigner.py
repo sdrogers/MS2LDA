54d37c944e3d85f431d8215c124d945b487fb17f
sdrogers <simon.d.rogers@gmail.com>
2015-10-12 11:17:45 +0100

    fixing merging problems
    Former-commit-id: 9723bb7b0312eaf07f0728f3731c0c6c94219fb0
    Former-commit-id: b1ee2c1c38e8c0237cdde2b06fb593d75216bc99

--- efcompute/ef_assigner.py
+++ efcompute/ef_assigner.py
@@ -1,6 +1,7 @@
 import sys
 import fractions
 from math import ceil,floor
+from golden_rules import golden_rules
 
 infinite = sys.maxint
 
@@ -11,24 +12,39 @@ atom_masses = {'C':12.00000000000,
                 'P':30.97376151200,
                 'S':31.97207069000}
 
+proton_mass = 1.00727645199076
+
 class ef_assigner(object):
-	def __init__(self,atoms = ['C','H','N','O','S','P'],scale_factor=1000):
+	def __init__(self,atoms = ['C','H','N','O','P','S'],scale_factor=1000,enforce_ppm = True,do_7_rules = True):
 		self.atoms = atoms
 		self.scale_factor = scale_factor
 		self.a = self.get_dictionary()
 		self.rr = self.round_robin()
-
+		self.enforce_ppm = enforce_ppm
+		self.do_7_rules = do_7_rules
 		# Compute correction factor for upper bound
 		self.delta = 0
 		for i in atoms:
 		    delta_i = (ceil(scale_factor*atom_masses[i]) - scale_factor*atom_masses[i])/atom_masses[i]
 		    if delta_i > self.delta:
 				self.delta = delta_i
+		print self.delta
 
-	def find_all(self,mass,i,c):
+	def find_all(self,mass,i,c,ppm,precursor_mass):
 	    if i == 0:
 	        c[0] = mass/self.a[0]
-	        formulas.append(list(c))
+	        # The following corrects for the fact that at low scale factors we
+	        # will find things at much more than the specificed ppm
+	        if self.enforce_ppm:
+	        	molecule_mass = 0.0
+	        	for i,atom in enumerate(self.atoms):
+	        		molecule_mass += atom_masses[atom]*c[i]
+        		if abs(molecule_mass - precursor_mass)/precursor_mass <= 1e-6*ppm:
+        			formulas.append(list(c))
+
+        		return
+	        else:
+	        	formulas.append(list(c))
 	        return
 	    else:
 	        lcm = self.a[0]*self.a[i] / fractions.gcd(self.a[0],self.a[i])
@@ -39,32 +55,41 @@ class ef_assigner(object):
 	            r = m % self.a[0]
 	            lbound = self.rr[i-1][r]
 	            while m >= lbound:
-	                self.find_all(m,i-1,c)
+	                self.find_all(m,i-1,c,ppm,precursor_mass)
 	                m = m - lcm
 	                c[i] = c[i] + l
 
 
-	def find_formulas(self,precursor_mass_list,ppm):
+	def find_formulas(self,precursor_mass_list,ppm = 5,polarisation="None"):
 		global formulas
 		print "Finding formulas at {}ppm".format(ppm)
 		formulas_out = {}
+		gr = golden_rules()
+		top_hit_string = []
 		for precursor_mass in precursor_mass_list:
 
+		    float_mass = float(precursor_mass)
 		    k = len(self.rr)
 		    c = [0 for i in range(0, k)]
 
-		    print "Searching for {}".format(precursor_mass)
+		    print "Searching for {}".format(float_mass)
 		    formulas = []
 
-		    ppm_error = ppm*precursor_mass/1e6
-		    lower_bound = precursor_mass - ppm_error
-		    upper_bound = precursor_mass + ppm_error
+		    if polarisation == "POS":
+		    	float_mass -= proton_mass
+		    elif polarisation == "NEG":
+		    	float_mass += proton_mass
+		    	
+		    ppm_error = ppm*float_mass/1e6
+		    lower_bound = float_mass - ppm_error
+		    upper_bound = float_mass + ppm_error
+
 
 		    int_lower_bound = int(ceil(lower_bound*self.scale_factor))
-		    int_upper_bound = int(floor(self.scale_factor*upper_bound + self.delta*upper_bound))
+		    int_upper_bound = int(floor(upper_bound*self.scale_factor + self.delta*upper_bound))
 
 		    for int_mass in range(int_lower_bound,int_upper_bound+1):
-		        self.find_all(int_mass,k-1,c)
+		        self.find_all(int_mass,k-1,c,ppm,float_mass)
 
 		    print "\t found {}".format(len(formulas))
 
@@ -74,9 +99,44 @@ class ef_assigner(object):
 		    	for i,a in enumerate(self.atoms):
 		    		formula[a] = f[i]
 		    	formulas_out[precursor_mass].append(formula)
-		    
 
-		return formulas_out
+		    if self.do_7_rules:
+		    	filtered_formulas_out = {}
+		    	filtered_formulas_out[precursor_mass],passed,failed = gr.filter_list(formulas_out[precursor_mass])
+		    	formulas_out[precursor_mass] = filtered_formulas_out[precursor_mass]
+		    
+		    if polarisation == "POS":
+		    	for f in formulas_out[precursor_mass]:
+		    		f['H'] += 1
+		    elif polarisation == "NEG":
+		    	for f in formulas_out[precursor_mass]:
+		    		f['H'] -= 1
+
+		    # If there is more than one hit return the top hit as a top_hit_string
+		    if len(formulas_out[precursor_mass]) == 0:
+		    	top_hit_string.append(None)
+		    	continue
+		    else:
+		    	closest = None
+		    	for f in formulas_out[precursor_mass]:
+		    		mass = 0.0
+		    		f_string = ""
+		    		for atom in f:
+		    			mass += f[atom]*atom_masses[atom]
+		    			if f[atom]>1:
+		    				f_string += "{}{}".format(atom,f[atom])
+		    			elif f[atom] == 1:
+		    				f_string += "{}".format(atom)
+		    		er = abs(mass - float_mass)
+		    		if closest == None:
+		    			best_er = er
+		    			closest = f_string
+	    			elif best_er < er:
+	    				closest = f_string
+	    		top_hit_string.append(f_string)
+
+
+		return formulas_out,top_hit_string
 
 
 	def get_dictionary(self):
