cc9adf51d8f50e31efb7d5cf6f9b279966bfd95b
Joe Wandy <joewandy@gmail.com>
2015-10-24 13:52:00 +0100

    Updated atom mass constants to more decimal places. Also updated the notebook to explain more about parameters to the EF assigners

--- efcompute/example.py
+++ efcompute/example.py
@@ -41,7 +41,7 @@ if __name__=='__main__':
         mass_list.append(get_formula_mass(atoms,test_molecule))
     
     # Create the ef_assigner object
-    ef = ef_assigner(scale_factor=1000, do_7_rules=True, 
+    ef = ef_assigner(scale_factor=100, do_7_rules=True, 
                      do_rule_8=True, rule_8_max_occurrences={'N':0, 'F':0, 'C':5})
 
     polarisation = "POS"    
